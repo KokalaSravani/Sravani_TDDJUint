96372508c9603f5e5c0fd29b50827569fcb02e45
KokalaSravani <17wh1a05c0@bvrithyderabad.edu.in>
2020-03-25 16:07:15 +0530

    This is my commit

---
+++ TddJUnit/src/main/java/epam/com/TddJUnit/RemoveA.java
@@ -0,0 +1,18 @@
+package epam.com.TddJUnit;
+
+public class RemoveA {
+	public String removeA(String string) {
+		String ans = string;
+		if(string.length() >= 1 && (string.charAt(0) == 'A' || string.charAt(0) == 'a')) {
+			ans = string.substring(1);
+		}
+		if(string.length() >= 2 && (string.charAt(0) != 'A'|| string.charAt(0) != 'a') && (string.charAt(1) == 'A' || string.charAt(1) == 'a' )) {
+			ans = string.charAt(0) + string.substring(2);
+		}
+		if(string.length() >= 2 && (string.charAt(0) == 'A' || string.charAt(0) == 'a')  && (string.charAt(1) == 'A' || string.charAt(1) == 'a' )) {
+			ans = string.substring(2);
+		}
+		return ans; 
+	}
+
+}
